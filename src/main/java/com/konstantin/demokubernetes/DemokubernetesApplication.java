5a1eb7ad8fe42d20a87317ee756572d0acd3b9ab
Stan Sh <shevchukstan@gmail.com>
2021-04-20 12:13:19 -0400

    Initial commit

---
+++ src/main/java/com/konstantin/demokubernetes/DemokubernetesApplication.java
@@ -0,0 +1,13 @@
+package com.konstantin.demokubernetes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemokubernetesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemokubernetesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/konstantin/demokubernetes/DemokubernetesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.konstantin.demokubernetes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemokubernetesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
